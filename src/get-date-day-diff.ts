8bce482eb807072efe4d2766a51eb7acf13a9006
Zenith Wogwugwu <uzenith360@gmail.com>
2022-12-18 19:43:46 +0100

    fix: actually refactor, i didnt some code refactoring but i want to force a build

--- src/get-date-day-diff.ts
+++ src/get-date-day-diff.ts
@@ -2,7 +2,7 @@
  * Get the difference in days between two dates
  */
 export default (date: Date, anotherDate:Date): number => {
-    const oneDay: number = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    const oneDay: number = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
     const firstDate: number = new Date(date).setHours(0, 0, 0, 0);
     const secondDate: number = new Date(anotherDate).setHours(0, 0, 0, 0);
     
