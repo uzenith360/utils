5a3105d486d15d767f4f77aac6270254b0765ba9
Zenith Wogwugwu <uzenith360@gmail.com>
2022-12-24 17:00:52 +0100

    test: fix capitalize function unit test description

--- tests/capitalize.spec.ts
+++ tests/capitalize.spec.ts
@@ -3,7 +3,7 @@ import { assert } from 'chai';
 import capitalize from '../src/capitalize';
 
 describe('Capitalize', () => {
-    it('should return a formatted money string', () => {
+    it('should return a capitalized string', () => {
         const expected = 'Zenith wogwugwu is 1% better.';
         const actual = capitalize('zenitH woGwugwU iS 1% BetTeR.');
 
