d9d94445f5f50f7ecad9ef4ae93237e418345d71
Zenith Wogwugwu <uzenith360@gmail.com>
2023-01-05 22:03:54 +0100

    fix: fix bug in method to get first day of quarter

--- src/get-first-day-of-quarter-date.ts
+++ src/get-first-day-of-quarter-date.ts
@@ -1,5 +1,5 @@
 export default (date: Date = new Date()): Date => {
-    const quarter: number = Math.floor((new Date().getMonth() + 3) / 3);
+    const quarter: number = Math.floor((date.getMonth() + 3) / 3);
 
     return new Date(date.getFullYear(), (quarter - 1) * 3, 1, 0, 0, 0, 0);
 }
