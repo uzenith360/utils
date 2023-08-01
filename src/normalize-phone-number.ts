eb194fade6c1b78b102b2e5a8ac315d37f358977
Zenith Wogwugwu <uzenith360@gmail.com>
2023-08-01 12:45:47 +0100

    fix: normalize phone number

--- src/normalize-phone-number.ts
+++ src/normalize-phone-number.ts
@@ -1,11 +1,15 @@
 export default (phoneNumber: string, countryCode: string = '234'): string => {
-    let phone: string = String(phoneNumber).trim()/*.replace(/\D/g, '')*/;
+    const phone: string = String(phoneNumber).replace(/[^\+0-9]/g, '')/*.trim()*//*.replace(/\D/g, '')*/;
 
     if (phone.charAt(0) === '+') {
         return phone;
-    } else if (phone.charAt(0) === '(') {
+    } /*else if (phone.charAt(0) === '(') {
         return `+${countryCode} ${phone}`;
-    } else if (phone.length < 12) {
+    }*/ else if (phone.length === 10) {
+        return `+${countryCode}${phone}`;
+    } /*else if (phone.length < 12) {
+        return `+${countryCode}${phone.substring(1)}`;
+    }*/ else if (phone.charAt(0) === '0') {
         return `+${countryCode}${phone.substring(1)}`;
     }
 
--- tests/normalize-phone-number.spec.ts
+++ tests/normalize-phone-number.spec.ts
@@ -6,7 +6,7 @@ import normalizePhoneNumber from '../src/normalize-phone-number';
 describe('Normalize phone number', () => {
     it('should normalize short phone number in default country code', () => {
         const expected = '+2348139498596';
-        const actual = normalizePhoneNumber('08139498596', '234');
+        const actual = normalizePhoneNumber('08139498596');
         assert.equal(actual, expected);
     });
 
@@ -28,14 +28,38 @@ describe('Normalize phone number', () => {
         assert.equal(actual, expected);
     });
 
+    it('should remove spaces', () => {
+        const expected = '+2348139498596';
+        const actual = normalizePhoneNumber(' +234 813 949 8596 ');
+        assert.equal(actual, expected);
+    });
+
+    it('should normalize nigerian number without country code', () => {
+        const expected = '+2348139498596';
+        const actual = normalizePhoneNumber('813 949 8596', '234');
+        assert.equal(actual, expected);
+    });
+
+    it('should normalize US number without country code', () => {
+        const expected = '+12124567890';
+        const actual = normalizePhoneNumber('212-456-7890', '1');
+        assert.equal(actual, expected);
+    });
+
+    it('should normalize US number with country code and +', () => {
+        const expected = '+12124567890';
+        const actual = normalizePhoneNumber('+1-212-456-7890', '1');
+        assert.equal(actual, expected);
+    });
+
     it('should normalize US phone number', () => {
-        const expected = '+1 (659) 599-5773';
+        const expected = '+16595995773'/*'+1 (659) 599-5773'*/;
         const actual = normalizePhoneNumber('1 (659) 599-5773', '1');
         assert.equal(actual, expected);
     });
 
     it('should leave full US phone number unchanged', () => {
-        const expected = '+1 (659) 599-5773';
+        const expected = '+16595995773'/*'+1 (659) 599-5773'*/;
         const actual = normalizePhoneNumber('+1 (659) 599-5773', '1');
         assert.equal(actual, expected);
     });
